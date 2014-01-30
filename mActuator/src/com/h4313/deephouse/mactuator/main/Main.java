0af32dc926922bbb5b012b5e09143f2a68c92673
macbric <macbric>
2014-01-30 23:59:39 +0100

    Mise en place de la réception et retransmission des messages

---
+++ mActuator/src/com/h4313/deephouse/mactuator/main/Main.java
@@ -0,0 +1,38 @@
+package com.h4313.deephouse.mactuator.main;
+
+import java.util.Scanner;
+
+import com.h4313.deephouse.mactuator.controller.Controller;
+import com.h4313.deephouse.util.DeepHouseCalendar;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		// Initialisation de l'horloge de simulation
+		DeepHouseCalendar.getInstance().init();
+		
+		// Initialisation du reseau
+		Controller.getInstance().initServerListener(Integer.valueOf(args[0]).intValue());
+		Controller.getInstance().initSensorSender(args[1], Integer.valueOf(args[2]).intValue());
+		Controller.getInstance().start();
+		
+		
+		// En attente de l'arret de la machine
+		String str = "";
+		Scanner scExit;
+		do {
+			scExit = new Scanner(System.in);
+			System.out.println("/// Tapez 'EXIT' pour arreter la machine ///");
+			str = scExit.nextLine();
+		} while (!str.toLowerCase().contains((CharSequence) "exit"));
+		scExit.close();
+	
+		Controller.getInstance().stopController();
+		
+		
+		System.out.println("Arret du serveur");
+
+		System.exit(0);
+	}
+}
